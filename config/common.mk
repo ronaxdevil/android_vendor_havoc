2f35ecb7109cad017689644d700a9849ed7ebd15
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    vendor: Port face unlock feature [2/4]
    Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6

--- config/common.mk
+++ config/common.mk
@@ -240,6 +240,16 @@ DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
 # Bootanimation
 include vendor/havoc/config/bootanimation.mk
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # Enable ccache
 USE_CCACHE := true
 
